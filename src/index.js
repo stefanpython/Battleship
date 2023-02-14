cc3efe4c3806ffa925c83918895a4fe44a3fd4e6
stefanpython <stefanhtml@yahoo.com>
2023-02-14 17:24:42 +0200

    Add game loop

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const Gameboard = require("../src/gameboard");
 const Player = require("../src/player");
 const createPlayerGrid = require("../src/playerGrid");
 const createAiGrid = require("../src/aiGrid");
+const dom = require("../src/DOM");
 
+// Create two visual gameboards, for ai one and for player one
 createPlayerGrid();
 createAiGrid();
+dom.playerEventHandler();
+// dom.aiEventHandler();
